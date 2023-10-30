9a4417de66dae719da02cfaf76f3b0fa5e82a52a
Antoine Fourmy <antoine.fourmy@gmail.com>
2023-10-30 08:22:24 -0400

    update

--- files/snippets/delete_soft_deleted_objects.py
+++ files/snippets/delete_soft_deleted_objects.py
@@ -1,2 +1,17 @@
 # Delete all soft-deleted workflow edges and services:
 # flake8: noqa
+
+db.delete(
+    "workflow_edge",
+    all_matches=True,
+    allow_none=True,
+    soft_deleted=True,
+)
+db.session.commit()
+db.delete(
+    "service",
+    all_matches=True,
+    allow_none=True,
+    soft_deleted=True,
+)
+db.session.commit()
