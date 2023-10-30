947e260730f58127b0c729e7319ac74e06a2ee6f
Antoine Fourmy <antoine.fourmy@gmail.com>
2023-10-30 08:29:42 -0400

    update soft deleted objects deletion script

--- files/snippets/delete_soft_deleted_objects.py
+++ files/snippets/delete_soft_deleted_objects.py
@@ -1,17 +1,13 @@
 # Delete all soft-deleted workflow edges and services:
 # flake8: noqa
 
-db.delete(
-    "workflow_edge",
-    all_matches=True,
-    allow_none=True,
-    soft_deleted=True,
-)
+soft_deleted_edges = db.fetch_all("workflow_edge", soft_deleted=True)
+for edge in soft_deleted_edges:
+    db.delete_instance(edge)
 db.session.commit()
-db.delete(
-    "service",
-    all_matches=True,
-    allow_none=True,
-    soft_deleted=True,
-)
+soft_deleted_services = db.fetch_all("service", soft_deleted=True)
+for service in soft_deleted_services:
+    db.delete_instance(service)
 db.session.commit()
+
+print(f"Soft-deleted objects successfully deleted")
