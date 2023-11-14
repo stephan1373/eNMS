28e9403d71bbf70829da7341773a555e8807159d
Antoine Fourmy <antoine.fourmy@gmail.com>
2023-11-14 00:16:43 -0500

    update black linting

--- eNMS/controller.py
+++ eNMS/controller.py
@@ -1485,7 +1485,9 @@ class Controller:
 
     def undo_log(self, log_id):
         log = db.fetch("changelog", id=log_id)
-        target = db.fetch(log.target_type, name=log.target_name, rbac="edit", allow_none=True)
+        target = db.fetch(
+            log.target_type, name=log.target_name, rbac="edit", allow_none=True
+        )
         if target is None:
             return {"alert": "The target object no longer exists."}
         for relationship, history in log.history.get("lists", {}).items():
