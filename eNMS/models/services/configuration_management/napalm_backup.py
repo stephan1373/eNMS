4e0268401b6c182c78fb41659c5786d06684b414
Antoine Fourmy <antoine.fourmy@gmail.com>
2024-02-07 08:47:49 -0500

    add dry run mode for napalm backup service

--- eNMS/models/services/configuration_management/napalm_backup.py
+++ eNMS/models/services/configuration_management/napalm_backup.py
@@ -36,6 +36,8 @@ class NapalmBackupService(ConnectionService):
 
     def job(self, run, device):
         local_path = run.sub(run.local_path, locals())
+        if run.dry_run:
+            return {"success": True, "local_path": local_path}
         path = Path.cwd() / local_path / device.name
         path.mkdir(parents=True, exist_ok=True)
         try:
