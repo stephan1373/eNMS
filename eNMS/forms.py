f23785dcd122466b4509763e11622155cbbc86ce
Antoine Fourmy <antoine.fourmy@gmail.com>
2024-06-03 12:49:38 -0400

    deviation 10: replace 'standby' with 'secondary' iin forms.py Server.role

--- eNMS/forms.py
+++ eNMS/forms.py
@@ -652,7 +652,7 @@ class ServerForm(BaseForm):
     name = StringField("Name", [InputRequired()])
     creator = StringField(render_kw={"readonly": True})
     description = StringField(widget=TextArea(), render_kw={"rows": 6})
-    role = SelectField("Role", choices=(("primary", "Primary"), ("standby", "Standby")))
+    role = SelectField("Role", choices=(("primary", "Primary"), ("secondary", "Secondary")))
     ip_address = StringField("IP address", render_kw={"readonly": True})
     scheduler_address = StringField("Scheduler Address", render_kw={"readonly": True})
     scheduler_active = BooleanField("Scheduler is Active", render_kw={"readonly": True})
