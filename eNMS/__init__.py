668f3172fe6867b2d040e2b44421a74c362f1a5d
Antoine Fourmy <antoine.fourmy@gmail.com>
2024-07-11 12:48:37 -0400

    fix init order: initialize server before plugins so plugins can use the csrf exempt decorator

--- eNMS/__init__.py
+++ eNMS/__init__.py
@@ -9,10 +9,10 @@ from eNMS.variables import vs
 
 
 def initialize():
-    server.register_plugins()
     env._initialize()
-    first_init = db._initialize(env)
     server._initialize()
+    server.register_plugins()
+    first_init = db._initialize(env)
     if env.detect_cli():
         return
     form_factory._initialize()
