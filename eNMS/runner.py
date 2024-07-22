3bbc8c8ae582de57ad5645dec414b55fd8eed2cb
Antoine Fourmy <antoine.fourmy@gmail.com>
2024-07-22 06:40:08 -0400

    fix safe_log function name

--- eNMS/runner.py
+++ eNMS/runner.py
@@ -1219,7 +1219,7 @@ class Runner:
         results = builtins[function](query, exec_variables) if query else ""
         return results, exec_variables
 
-    def safe_str(self, original, modified):
+    def safe_log(self, original, modified):
         if "get_secret" in original or "get_credential" in original:
             return original
         else:
