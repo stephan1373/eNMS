7ff74a01994e2337b68b1949012fd28173c05602
Antoine Fourmy <antoine.fourmy@gmail.com>
2024-11-20 17:03:42 -0500

    profiling p8

--- files/snippets/profiling.py
+++ files/snippets/profiling.py
@@ -1,6 +1,12 @@
 # Sort and display the profiling data stored in vs.profiling
 
-sorted_data = sorted(vs.profiling.items(), key=lambda x: x[1]["count"], reverse=True)
+# The functions can be sorted by:
+# - "count": how many times a function was called
+# - "average_time": how much time does the function take on average
+# - "combined_time": how much time did the function take in total
+SORT_BY = "count"
+
+sorted_data = sorted(vs.profiling.items(), key=lambda item: item[1][SORT_BY], reverse=True)
 
 print(
     "\n\n".join(
