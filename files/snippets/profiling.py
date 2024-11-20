9f2b60f3b10fe681ff7391942d2f78d6fd48bc87
Antoine Fourmy <antoine.fourmy@gmail.com>
2024-11-20 16:56:35 -0500

    profiling p5

--- files/snippets/profiling.py
+++ files/snippets/profiling.py
@@ -1,13 +1,10 @@
 # Sort and display the profiling data stored in vs.profiling
 
-sorted_data = sorted(
-    vs.profiling.items(),
-    key=lambda x: x[1]['count'],
-    reverse=True
-)
+sorted_data = sorted(vs.profiling.items(), key=lambda x: x[1]["count"], reverse=True)
 
-print("\n\n".join(
-	f"Function '{name}':\n"
-    f"{vs.dict_to_string(data, depth=2)}"
-  for name, data in sorted_data
-))
+print(
+    "\n\n".join(
+        f"Function '{name}':\n {vs.dict_to_string(data, depth=2)}"
+        for name, data in sorted_data
+    )
+)
