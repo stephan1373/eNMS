82a67688435ac8c25521bc8b5e7cc7e48961dd1e
Antoine Fourmy <antoine.fourmy@gmail.com>
2025-02-11 20:01:55 -0500

    datastore 87

--- eNMS/models/datastore/ip_address.py
+++ eNMS/models/datastore/ip_address.py
@@ -2,7 +2,7 @@ from sqlalchemy import ForeignKey, Integer
 
 from eNMS.database import db
 from eNMS.forms import DataForm
-from eNMS.fields import HiddenField
+from eNMS.fields import HiddenField, SelectField, StringField
 from eNMS.models.administration import Data
 
 
@@ -11,7 +11,20 @@ class IPAddress(Data):
     pretty_name = "IP Address"
     __mapper_args__ = {"polymorphic_identity": "ip_address"}
     id = db.Column(Integer, ForeignKey("data.id"), primary_key=True)
+    address = db.Column(db.TinyString)
+    role = db.Column(db.TinyString)
+    vrf_instance = db.Column(db.SmallString)
 
 
 class IPAdressForm(DataForm):
     form_type = HiddenField(default="ip_address")
+    address = StringField()
+    role = SelectField(
+        "Role",
+        choices=(
+            ("loopback", "Loopback"),
+            ("secondary", "Secondary"),
+            ("anycast", "Anycast"),
+        ),
+    )
+    vrf_instance = StringField()
