c1f726063cdf8ed3013d4eb6ae3cf16f441c59ed
Antoine Fourmy <antoine.fourmy@gmail.com>
2025-02-12 08:42:40 -0500

    datastore 95

--- eNMS/static/js/datastore/secret.js
+++ eNMS/static/js/datastore/secret.js
@@ -1,5 +1,4 @@
 import { tables } from "../table.js";
 
-tables.secret = class SecretTable extends tables.data {};
-
+tables.secret = class extends tables.data {};
 tables.secret.prototype.type = "secret";
