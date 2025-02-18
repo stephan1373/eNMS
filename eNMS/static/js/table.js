56fd759f5a4baff1d9c7aaad5e3ea7c9430f9a5d
Antoine Fourmy <antoine.fourmy@gmail.com>
2025-02-18 18:07:22 -0500

    datastore 139

--- eNMS/static/js/table.js
+++ eNMS/static/js/table.js
@@ -1671,31 +1671,31 @@ tables.data = class DataTable extends Table {
     return [
       `
       <ul class="pagination pagination-lg" style="margin: 0px;">
-        ${this.rowButtons(row)}
+        ${this.rowButtons(row).join("")}
       </ul>`,
     ];
   }
 
   rowButtons(row) {
-    return `
-      <li>
+    return [
+      `<li>
         <button type="button" class="btn btn-sm btn-info"
           onclick="eNMS.base.copyToClipboard({text: '${row.persistent_id}' })"
           data-tooltip="Copy Persistent ID to clipboard"
         >
           <span class="glyphicon glyphicon-copy"></span>
         </button>
-      </li>
-      <li>
+      </li>`,
+      `<li>
         <button type="button" class="btn btn-sm btn-primary"
           onclick="eNMS.base.showInstancePanel('${row.type}', '${row.id}')"
           data-tooltip="Edit"
         >
           <span class="glyphicon glyphicon-edit"></span>
         </button>
-      </li>
-      ${this.deleteInstanceButton(row)}
-    `;
+      </li>`,
+      this.deleteInstanceButton(row),
+    ];
   }
 
   postProcessing(...args) {
