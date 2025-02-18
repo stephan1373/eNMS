55ce00ad8de985ff36ff98234e88975e719dfc9b
Antoine Fourmy <antoine.fourmy@gmail.com>
2025-02-18 18:00:25 -0500

    datastore 136

--- eNMS/static/js/datastore/json.js
+++ eNMS/static/js/datastore/json.js
@@ -1,4 +1,36 @@
+/*
+global
+JSONEditor: false
+*/
+
+import { call, configureNamespace } from "../base.js";
 import { tables } from "../table.js";
 
-tables.json = class extends tables.data {};
+tables.json = class extends tables.data {
+  rowButtons(row) {
+    return `
+      <li>
+        <button type="button" class="btn btn-sm btn-info"
+          onclick="eNMS.datastore.downloadJSONObject('${row.id}')"
+          data-tooltip="View JSON Value"
+        >
+          <span class="glyphicon glyphicon-list-alt"></span>
+        </button>
+      </li>
+      ${super.rowButtons(row)}
+    `;
+  }
+};
+
+function downloadJSONObject(id) {
+  call({
+    url: `/get/json/${id}`,
+    callback: (result) => {
+      console.log(result)
+    },
+  });
+}
+
 tables.json.prototype.type = "json";
+
+configureNamespace("datastore", [downloadJSONObject]);
