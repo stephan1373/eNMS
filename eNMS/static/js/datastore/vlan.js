cb3a0b154b7ff9a077b43ee2e90bff770759f9bd
Antoine Fourmy <antoine.fourmy@gmail.com>
2025-02-19 23:02:49 -0500

    datastore 151

--- eNMS/static/js/datastore/vlan.js
+++ eNMS/static/js/datastore/vlan.js
@@ -1,4 +1,20 @@
+import { currentStore } from "../administration.js";
+import { call, configureNamespace, notify } from "../base.js";
 import { tables } from "../table.js";
 
 tables.vlan = class extends tables.data {};
-tables.vlan.prototype.type = "vlan";
+
+function getNextVlanId() {
+    call({
+      url: "/filtering/vlan",
+      data: {constraints: {store: [currentStore.name] }, bulk: "vlan_id"},
+      callback: (vlanIds) => {
+        notify(`Next Vlan ID: ${Math.max(...vlanIds) + 1}`, "success", 5);
+      },
+    });
+  }
+  
+  tables.vlan.prototype.type = "vlan";
+  
+  configureNamespace("datastore", [getNextVlanId]);
+  
