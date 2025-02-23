663806a9985f3ddc724992b07ba5d1e4bbf65a87
Antoine Fourmy <antoine.fourmy@gmail.com>
2025-02-22 20:41:21 -0500

    datastore 168

--- eNMS/models/datastore/DCIM/port.py
+++ eNMS/models/datastore/DCIM/port.py
@@ -1,4 +1,6 @@
 from sqlalchemy import Boolean, ForeignKey, Integer
+from sqlalchemy.ext.associationproxy import association_proxy
+from sqlalchemy.orm import relationship
 
 from eNMS.database import db
 from eNMS.forms import DataForm
@@ -14,12 +16,16 @@ class Port(Data):
     label = db.Column(db.SmallString)
     speed = db.Column(db.SmallString)
     connected = db.Column(Boolean, default=False)
+    device_id = db.Column(Integer, ForeignKey("device.id"))
+    device = relationship("Device")
+    device_name = association_proxy("device", "name")
 
 
 class PortForm(DataForm):
     form_type = HiddenField(default="port")
     store = InstanceField("Store", model="store", constraints={"data_type": "port"})
+    device = InstanceField("Device", model="device")
     label = StringField()
     speed = SelectField(choices=("10BASE-T", "100BASE-T", "1000BASE-T", "10GBASE-T"))
     connected = BooleanField("Connected", default=False)
-    properties = ["label", "speed", "connected"]
+    properties = ["device", "label", "speed", "connected"]
