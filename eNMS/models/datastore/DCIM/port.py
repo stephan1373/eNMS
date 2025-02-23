9402205ed6a8edb5ce0c724450d3f220715e09fe
Antoine Fourmy <antoine.fourmy@gmail.com>
2025-02-22 20:19:53 -0500

    datastore 160

---
+++ eNMS/models/datastore/DCIM/port.py
@@ -0,0 +1,25 @@
+from sqlalchemy import ForeignKey, Integer
+
+from eNMS.database import db
+from eNMS.forms import DataForm
+from eNMS.fields import HiddenField, InstanceField, StringField
+from eNMS.models.administration import Data
+
+
+class Port(Data):
+    __tablename__ = "port"
+    pretty_name = "Port"
+    __mapper_args__ = {"polymorphic_identity": "port"}
+    id = db.Column(Integer, ForeignKey("data.id"), primary_key=True)
+    label = db.Column(db.SmallString)
+    speed = db.Column(db.SmallString)
+    connected = db.Column(Boolean, default=False)
+
+
+class PortForm(DataForm):
+    form_type = HiddenField(default="port")
+    store = InstanceField("Store", model="store", constraints={"data_type": "port"})
+    label = StringField()
+    speed = StringField()
+    connected = BooleanField("Connected", default=False)
+    properties = ["label", "speed", "connected"]
