1806fd2bf039d08dfbfa0fbf830648bd168b1e39
Antoine Fourmy <antoine.fourmy@gmail.com>
2025-05-13 07:48:46 -0400

    json import export mechanism p6

--- eNMS/models/devices/generic_device.py
+++ eNMS/models/devices/generic_device.py
@@ -2,7 +2,7 @@ from sqlalchemy import ForeignKey, Integer
 
 from eNMS.database import db
 from eNMS.forms import DeviceForm
-from eNMS.fields import HiddenField, StringField
+from eNMS.fields import HiddenField
 from eNMS.models.inventory import Device
 
 
@@ -11,10 +11,7 @@ class GenricDevice(Device):
     __mapper_args__ = {"polymorphic_identity": "generic_device"}
     pretty_name = "Generic Device"
     id = db.Column(Integer, ForeignKey("device.id"), primary_key=True)
-    serial_number = db.Column(db.SmallString)
 
 
 class GenericDeviceForm(DeviceForm):
     form_type = HiddenField(default="generic_device")
-    serial_number = StringField("Serial Number")
-    properties = ["serial_number"]
