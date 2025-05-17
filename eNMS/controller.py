eaa282fda5cfb2c58ad43902dd7b1b82148146ff
Antoine Fourmy <antoine.fourmy@gmail.com>
2025-05-16 23:30:57 -0400

    json import export mechanism p95

--- eNMS/controller.py
+++ eNMS/controller.py
@@ -1138,21 +1138,11 @@ class Controller(vs.TimingMixin):
             if class_name not in db.json_migration["no_export"]
         ]
         path = Path(vs.migration_path) / kwargs["name"]
-        if kwargs.get("multithreading"):
-            with ThreadPoolExecutor(max_workers=10) as executor:
-                for cls_name in export_models:
-                    executor.submit(self.json_export_properties, cls_name, path)
-                    executor.submit(self.json_export_scalar, cls_name, path)
-                for association_name in db.associations:
-                    executor.submit(
-                        self.json_export_association, association_name, path
-                    )
-        else:
-            for cls_name in export_models:
-                self.json_export_properties(cls_name, path)
-                self.json_export_scalar(cls_name, path)
-            for association_name in db.associations:
-                self.json_export_association(association_name, path)
+        for cls_name in export_models:
+            self.json_export_properties(cls_name, path)
+            self.json_export_scalar(cls_name, path)
+        for association_name in db.associations:
+            self.json_export_association(association_name, path)
         with open("metadata.json", "wb") as file:
             metadata = {"version": vs.server_version, "export_time": datetime.now()}
             file.write(dumps(metadata))
