b035e2ca7cf917fb43bf6fcc19c5812600331551
Antoine Fourmy <antoine.fourmy@gmail.com>
2025-05-21 13:33:08 -0400

    json import export mechanism p145

--- files/scripts/generate_migration.py
+++ files/scripts/generate_migration.py
@@ -13,14 +13,14 @@ PATH = (
 def generate_devices():
     with open(PATH / "generic_device.json", "wb") as file:
         file.write(dumps([
-        {"name": f"d{index}", "model": f"Model{randrange(1, 21)}"}
+        {"name": f"Device{index}", "model": f"Model{randrange(1, 21)}"}
         for index in range(150_000)
     ]))
 
 def generate_links():
     with open(PATH / "generic_link.json", "wb") as file:
         file.write(dumps([
-            {"name": f"l{index}", "model": f"Model{randrange(1, 51)}"}
+            {"name": f"Link{index}", "model": f"Model{randrange(1, 51)}"}
             for index in range(30_000)
         ]))
 
@@ -46,10 +46,10 @@ def generate_services():
     with open(PATH / "python_snippet_service.json", "wb") as file:
         file.write(dumps([
             {
-                "name": f"[Shared] s{index}",
+                "name": f"[Shared] Service{index}",
                 "model": f"Model{randrange(1, 21)}",
                 "vendor": choice(["Cisco", "Juniper", "Arista"]),
-                "scoped_name": f"s{index}",
+                "scoped_name": f"Service{index}",
                 "shared": True
             } for index in range(9_997)
         ]))
@@ -59,14 +59,14 @@ def generate_workflows():
     for index in range(2_000):
         positions = {"[Shared] Start": [0, 0], "[Shared] End": [2200, 400]}
         for i in range(10):
-            positions[f"[Shared] s{index + i}"] = ((i + 1) * 200, 0)
-            positions[f"[Shared] s{index + i + 10}"] = (2000 - 200 * i, 200)
-            positions[f"[Shared] s{index + i + 20}"] = ((i + 1) * 200, 400)
+            positions[f"[Shared] Service{index + i}"] = ((i + 1) * 200, 0)
+            positions[f"[Shared] Service{index + i + 10}"] = (2000 - 200 * i, 200)
+            positions[f"[Shared] Service{index + i + 20}"] = ((i + 1) * 200, 400)
         workflows.append({
-            "name": f"[Shared] w{index}",
+            "name": f"[Shared] Workflow{index}",
             "positions": positions,
             "vendor": choice(["Cisco", "Juniper", "Arista"]),
-            "scoped_name": f"w{index}",
+            "scoped_name": f"Workflow{index}",
             "shared": True,
         })
     with open(PATH / "workflow.json", "wb") as file:
@@ -78,7 +78,7 @@ def generate_workflow_association_table():
         association_table.append(["[Shared] Start", f"[Shared] w{j}"])
         association_table.append(["[Shared] End", f"[Shared] w{j}"])
         for i in range(j, j + 30):
-            association_table.append([f"[Shared] s{i}", f"[Shared] w{j}"]) 
+            association_table.append([f"[Shared] Service{i}", f"[Shared] Workflow{j}"]) 
     with open(PATH / "service_workflow_table.json", "wb") as file:
         file.write(dumps(association_table))
 
