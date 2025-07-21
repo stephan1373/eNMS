97e111471093dcdb2925d4c4df18f7fcd7752228
Antoine Fourmy <antoine.fourmy@gmail.com>
2025-07-21 13:15:29 +0000

    scan folder optimization p4

--- files/snippets/scan_folder_create_10K_files.py
+++ files/snippets/scan_folder_create_10K_files.py
@@ -7,6 +7,6 @@ from pathlib import Path
 path_folder = vs.file_path / "test"
 path_folder.mkdir(exist_ok=True)
 
-for i in range(1, 1000001):
+for i in range(10000):
     file_path = path_folder / f"file{i}"
     file_path.touch()
