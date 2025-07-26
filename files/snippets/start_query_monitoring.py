79c0f5cc3bd92ca88aa7d58b73d5c3ee4b69cea4
Antoine Fourmy <antoine.fourmy@gmail.com>
2025-07-26 09:13:50 +0000

    remove existing snippets

--- files/snippets/start_query_monitoring.py
+++
@@ -1,8 +0,0 @@
-# flake8: noqa
-# This script starts the monitoring mechanism for SQL queries
-# and clears the content of the dict used for the analysis.
-
-db.monitor_orm_statements = True
-db.orm_statements.clear()
-db.orm_statements_runtime.clear()
-db.orm_statements_tracebacks.clear()
