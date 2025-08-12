f5222f0dfb3d485ac2c361af6f36b71381b47edc
Antoine Fourmy <antoine.fourmy@gmail.com>
2025-08-12 12:50:36 +0000

    reorder p29

--- eNMS/custom.py
+++ eNMS/custom.py
@@ -13,43 +13,6 @@ from eNMS.variables import vs
 
 
 class CustomApp(vs.TimingMixin):
-    def ldap_authentication(self, user, name, password):
-        if not hasattr(env, "ldap_servers"):
-            env.log("error", "LDAP authentication failed: no server configured")
-            return False
-        user = f"uid={name},dc=example,dc=com"
-        success = Connection(env.ldap_servers, user=user, password=password).bind()
-        return {"name": name, "is_admin": True} if success else False
-
-    def tacacs_authentication(self, user, name, password):
-        if not hasattr(env, "tacacs_client"):
-            env.log("error", "TACACS+ authentication failed: no server configured")
-            return False
-        success = env.tacacs_client.authenticate(name, password).valid
-        return {"name": name, "is_admin": True} if success else False
-
-    def parse_configuration_property(self, device, property, value=None):
-        if not value:
-            value = getattr(device, property)
-        if device.operating_system == "EOS" and property == "configuration":
-            value = sub(r"(username.*secret) (.*)", "\g<1> ********", value)
-        return value
-
-    def generate_uuid(self):
-        return str(uuid4())
-
-    def log_post_processing(_self, **kwargs):
-        return
-
-    def run_post_processing(self, run, run_result):
-        return
-
-    def runner_global_variables(self, run):
-        return {}
-
-    def server_template_context(self):
-        return {}
-
     def get_yaml_instance(self):
         yaml = YAML(typ="safe")
         yaml.default_style = '"'
@@ -68,6 +31,43 @@ class CustomApp(vs.TimingMixin):
         yaml.representer.add_representer(str, representer)
         yaml.representer.ignore_aliases = lambda *args: True
         return yaml
+
+    def generate_uuid(self):
+        return str(uuid4())
+
+    def ldap_authentication(self, user, name, password):
+        if not hasattr(env, "ldap_servers"):
+            env.log("error", "LDAP authentication failed: no server configured")
+            return False
+        user = f"uid={name},dc=example,dc=com"
+        success = Connection(env.ldap_servers, user=user, password=password).bind()
+        return {"name": name, "is_admin": True} if success else False
+
+    def log_post_processing(_self, **kwargs):
+        return
+
+    def parse_configuration_property(self, device, property, value=None):
+        if not value:
+            value = getattr(device, property)
+        if device.operating_system == "EOS" and property == "configuration":
+            value = sub(r"(username.*secret) (.*)", "\g<1> ********", value)
+        return value
+
+    def tacacs_authentication(self, user, name, password):
+        if not hasattr(env, "tacacs_client"):
+            env.log("error", "TACACS+ authentication failed: no server configured")
+            return False
+        success = env.tacacs_client.authenticate(name, password).valid
+        return {"name": name, "is_admin": True} if success else False
+
+    def run_post_processing(self, run, run_result):
+        return
+
+    def runner_global_variables(self, run):
+        return {}
+
+    def server_template_context(self):
+        return {}
 
 
 vs.custom = CustomApp()
