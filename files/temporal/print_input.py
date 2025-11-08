36b1ed183bbe3e53b7abf8c27c799363e3c5991a
Antoine Fourmy <antoine.fourmy@gmail.com>
2025-11-08 04:04:50 +0000

    rename print input temporal worker

--- files/temporal/print_input.py
+++ files/temporal/print_input.py
@@ -7,15 +7,15 @@ from temporalio.worker import Worker
 
 
 @activity.defn
-async def say_hello(name):
+async def print_input(name):
     return f"Hi {name}!"
 
 @workflow.defn
-class HelloWorkflow:
+class PrintInput:
     @workflow.run
     async def run(self, name):
         result = await workflow.execute_activity(
-            say_hello,
+            print_input,
             name,
             start_to_close_timeout=timedelta(seconds=10),
         )
@@ -26,9 +26,9 @@ async def main():
     client = await Client.connect("localhost:7233")
     worker = Worker(
         client,
-        task_queue="hello-task-queue",
-        workflows=[HelloWorkflow],
-        activities=[say_hello],
+        task_queue="print-input-queue",
+        workflows=[PrintInput],
+        activities=[print_input],
     )
     await worker.run()
 
