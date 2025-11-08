1bb1a9bf7f7585e21e180cb7393e301c9a2b1b37
Antoine Fourmy <antoine.fourmy@gmail.com>
2025-11-08 09:58:15 +0000

    update temporal p15

--- eNMS/models/services/temporal/stop_temporal_workflow.py
+++ eNMS/models/services/temporal/stop_temporal_workflow.py
@@ -9,7 +9,7 @@ except ImportError as exc:
     warn(f"Couldn't import temporalio.client module ({exc})")
 
 from eNMS.database import db
-from eNMS.fields import HiddenField, StringField
+from eNMS.fields import HiddenField, SelectField, StringField
 from eNMS.forms import ServiceForm
 from eNMS.models.automation import Service
 from eNMS.variables import vs
@@ -22,6 +22,8 @@ class TemporalStopWorkflowService(Service):
     server_url = db.Column(db.SmallString)
     workflow_id = db.Column(db.SmallString)
     run_id = db.Column(db.SmallString)
+    stop_method = db.Column(db.SmallString)
+    terminate_reason = db.Column(db.SmallString)
     
     __mapper_args__ = {"polymorphic_identity": "stop_temporal_workflow_service"}
     
@@ -31,17 +33,21 @@ class TemporalStopWorkflowService(Service):
         result = {
             "workflow_id": run.sub(run.workflow_id, local_variables),
             "run_id": run.sub(run.run_id, local_variables),
-            "status": "Cancel",
+            "terminate_reason": run.sub(run.terminate_reason, local_variables),
+            "status": run.stop_method,
         }
         run.log("info", f"Stopping Temporal workflow: {result['workflow_id']}", device)
         if run.dry_run:
-            return {"url": url, **kwargs}
+            return {"url": url, **result}
 
         async def stop_workflow():
             client = await Client.connect(url)
             kwargs = {"run_id": result["run_id"]} if result["run_id"] else {}
             handle = client.get_workflow_handle(result["workflow"], **kwargs)
-            await handle.cancel()
+            if run.stop_method == "cancel":
+                await handle.cancel()
+            else:
+                await handle.terminate(reason=result["terminate_reason"])
             return result
 
         return asyncio_run(stop_workflow())
@@ -57,3 +63,8 @@ class TemporalStopWorkflowForm(ServiceForm):
     )
     workflow_id = StringField("Workflow ID", [InputRequired()], substitution=True)
     run_id = StringField("Run ID (Optional)", substitution=True)
+    stop_method = SelectField(
+        "Stop Method",
+        choices=[("cancel", "Cancel (Graceful)"), ("terminate", "Terminate (Force)")],
+    )
+    terminate_reason = StringField("Terminate Reason (Optional)", substitution=True)
