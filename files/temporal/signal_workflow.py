70518f407ae6541306582b992ff39f5c0a12e55c
Antoine Fourmy <antoine.fourmy@gmail.com>
2025-11-12 17:08:17 +0000

    update temporal p43

---
+++ files/temporal/signal_workflow.py
@@ -0,0 +1,49 @@
+from asyncio import run
+from datetime import timedelta
+
+from temporalio import workflow, activity
+from temporalio.client import Client
+
+
+@activity.defn
+async def process_order():
+    activity.logger.info(f"Processed")
+    return f"Processed"
+
+@workflow.defn
+class SignalWorkflow:
+    def __init__(self):
+        self.approved = False
+    
+    @workflow.signal
+    async def approve(self):
+        self.approved = True
+    
+    @workflow.run
+    async def run(self):
+        workflow.logger.info("Before Wait Condition")
+        await workflow.wait_condition(lambda: self.approved, timeout=timedelta(hours=1))
+        workflow.logger.info("After Wait Condition")
+        result = await workflow.execute_activity(
+            process_order,
+            args=[],
+            start_to_close_timeout=timedelta(minutes=5)
+        )
+        return result
+
+async def main():
+    client = await Client.connect("localhost:7233")
+    await Worker(
+        client,
+        task_queue="signal-queue",
+        workflows=[SignalWorkflow],
+        activities=[process_order],
+    ).run()
+
+
+async def approve_order(workflow_id: str):
+    client = await Client.connect("localhost:7233")
+    await client.get_workflow_handle(workflow_id).signal("approve")
+
+if __name__ == "__main__":
+    run(main())
