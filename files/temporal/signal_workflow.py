2857c6ddf50ff3efe79e925f92b228b211b960b4
Antoine Fourmy <antoine.fourmy@gmail.com>
2025-11-12 17:17:49 +0000

    update temporal p47

--- files/temporal/signal_workflow.py
+++ files/temporal/signal_workflow.py
@@ -9,26 +9,26 @@ from temporalio.worker import Worker
 @workflow.defn
 class SignalWorkflow:
     def __init__(self):
-        self.approved = False
+        self.value = None
     
     @workflow.signal
-    async def approve(self):
-        self.approved = True
+    async def set_value(self, new_value):
+        self.value = new_value
     
     @workflow.run
     async def run(self):
-        workflow.logger.info("Before Wait Condition")
-        await workflow.wait_condition(lambda: self.approved, timeout=timedelta(hours=1))
-        workflow.logger.info("After Wait Condition")
-        return "Workflow Completed"
+        workflow.logger.info("Waiting for value...")
+        await workflow.wait_condition(lambda: self.value is not None, timeout=timedelta(hours=1))
+        workflow.logger.info(f"Received value: {self.value}")
+        return f"Workflow completed with value: {self.value}"
 
 async def main():
     client = await Client.connect("localhost:7233")
     await Worker(client, task_queue="signal-queue", workflows=[SignalWorkflow]).run()
 
-async def send_signal(workflow_id: str):
+async def send_signal(workflow_id: str, value):
     client = await Client.connect("localhost:7233")
-    await client.get_workflow_handle(workflow_id).signal("approve")
+    await client.get_workflow_handle(workflow_id).signal("set_value", value)
 
 if __name__ == "__main__":
     run(main())
