c48059f8182e0fa0bf915c11c1ea822b31415006
Antoine Fourmy <antoine.fourmy@gmail.com>
2025-11-13 17:55:08 +0000

    always call 'post_update' in Controller.update otherwise Service.builder_link is not set the run URL in the Results page does not work because path is empty

--- eNMS/controller.py
+++ eNMS/controller.py
@@ -1791,6 +1791,7 @@ class Controller(vs.TimingMixin):
                     builder_id = kwargs[f"{builder_type}s"][0]
                     db.fetch(builder_type, id=builder_id, rbac="edit")
             instance = db.factory(type, commit=True, **kwargs)
+            instance.post_update()
             if kwargs.get("copy"):
                 db.fetch(type, id=kwargs["copy"]).duplicate(clone=instance)
             if relations := vs.properties["update"].get(instance.class_type):
