b21cd04a9b9ff50e7f153b044be16b68703c9bbf
Antoine Fourmy <antoine.fourmy@gmail.com>
2025-12-25 11:09:11 +0000

    add server scalability script p2

--- files/scripts/test_server_scalability.py
+++ files/scripts/test_server_scalability.py
@@ -1,16 +1,20 @@
-from requests import get
 from concurrent.futures import ThreadPoolExecutor
+from requests import get
+from requests.auth import HTTPBasicAuth
+
 
 BASE_URL = "http://127.0.0.1:5000/rest"
+auth = HTTPBasicAuth("admin", "admin")
 
 def send_is_alive_request():
     if get(f"{BASE_URL}/is_alive").status_code != 200:
         print("error")
 
 def send_get_instance_request():
-    if get(f"{BASE_URL}/get_instance/device/Washington").status_code != 200:
-        print("error")
+    request = get(f"{BASE_URL}/instance/device/Washington", auth=auth)
+    if request.status_code != 200:
+        print(request)
 
 with ThreadPoolExecutor(max_workers=10) as pool:
-    for _ in range(10_000):
+    for _ in range(100):
         pool.submit(send_get_instance_request)
